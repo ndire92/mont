3647e45b80e1ad61eaf6fa927cc688e6fdd74638
ndir92 <mamadoundir2@gmail.com>
2023-04-06 02:04:15 +0000

    first commit

--- Stud/settings.py
+++ Stud/settings.py
@@ -82,22 +82,22 @@ WSGI_APPLICATION = 'Stud.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
-#DATABASES = {
-  #  'default': {
-   #     'ENGINE': 'django.db.backends.mysql',
-   #    'NAME': 'agri',
-      #  'USER': 'root',
-      #  'PASSWORD': 'root',
-      #  'HOST': 'localhost',
-      #  'PORT': '3306',
-    ##}
-#}
+# DATABASES = {
+#  'default': {
+#     'ENGINE': 'django.db.backends.mysql',
+#    'NAME': 'agri',
+#  'USER': 'root',
+#  'PASSWORD': 'root',
+#  'HOST': 'localhost',
+#  'PORT': '3306',
+# }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -146,4 +146,4 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'app.CustomUser'
+AUTH_USER_MODEL = 'auth.User'
